1ba2d26c7e16b50ebe8e01ff0e51ec8141e4e2dc
Ed <Ed@Eds-MacBook-Air.local>
2017-10-03 13:20:23 -0400

    InfoController corrected

--- app/Http/Controllers/InfoController.php
+++ app/Http/Controllers/InfoController.php
@@ -6,109 +6,56 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Response;
 use Purifier;
-use App\Company;
-use App\Filter;
-use App\Investment;
-use App\User;
-use App\Role;
+
+use App\Option;
 
 class InfoController extends Controller
 {
-  public function saveInvestmentInfo(Request $request)
+  public function saveOptions(Request $request)
   {
     $rules = [
-      'userID' => 'required',
-      'name' => 'required',
-      'age' => 'required',
-      'goal' => 'required',
-      'amount' => 'required',
-      'reoccurring' => 'required',
-      'contribution' => 'required',
-      'horizon_num' => 'required',
-      'horizon_type' => 'required'
+      'minInvestment'=> 'required',
+      'riskLevel'=> 'required'
     ];
-    $validator = Validator::make(Purifier::clean($request->all()), $rules);
 
+    $validator = Validator::make(Purifier::clean($request->all()), $rules);
     if($validator->fails())
     {
-      return Response::json(['error' => 'Please fill out all required fields.']);
+      return Response::json(['error' => 'Missing fields']);
     }
-    $userID = $request->input('userID');
-    $name = $request->input('name');
-    $age = $request->input('age');
-    $goal = $request->input('goal');
-    $amount = $request->input('amount');
-    $reoccurring = $request->input('reoccurring');
-    $contribution = $request->input('contribution');
-    $horizon_num = $request->input('horizon_num');
-    $horizon_type = $request->input('horizon_type');
+
     $stocks = $request->input('stocks');
-    $options = $request->input('options');
-    $exTradeFunds = $request->input('exTradeFunds');
-    $secFutures = $request->input('secFutures');
     $bonds = $request->input('bonds');
     $mutualFunds = $request->input('mutualFunds');
-    $collegeSavings = $request->input('collegeSavings');
-    $commodityFutures = $request->input('commodityFutures');
+    $etfs = $request->input('etfs');
+    $indexFunds = $request->input('indexFunds');
+    $retirement = $request->input('retirement');
+    $minInvestment = $request->input('minInvestment');
+    $riskLevel = $request->input('riskLevel');
 
-    $info = new Investment;
-    $info->userID = $userID;
-    $info->name = $name;
-    $info->age = $age;
-    $info->goal = $goal;
-    $info->amount = $amount;
-    $info->reoccurring = $reoccurring;
-    $info->contribution = $contribution;
-    $info->horizon_num = $horizon_num;
-    $info->horizon_type = $horizon_type;
-    $info->stocks = $stocks;
-    $info->options = $options;
-    $info->exTradeFunds = $exTradeFunds;
-    $info->secFutures = $secFutures;
-    $info->bonds = $bonds;
-    $info->mutualFunds = $mutualFunds;
-    $info->collegeSavings = $collegeSavings;
-    $info->commodityFutures = $commodityFutures;
-    $info->save();
+    $options = new Option;
 
-    return Response::json(['info' => $info]);
+    $options->stocks = $stocks;
+    $options->bonds = $bonds;
+    $options->mutualFunds = $mutualFunds;
+    $options->etfs = $etfs;
+    $options->indexFunds = $indexFunds;
+    $options->retirement = $retirement;
+    $options->minInvestment = $minInvestment;
+    $options->riskLevel = $riskLevel;
+    $options->save();
+
+    return Response::json(['success' => "Data saved"]);
   }
 
-  public function displayResults(Request $request)
+
+  public function displayResults()
   {
-    $userID = $request->input('userID');
-    $name = $request->input('name');
-    $age = $request->input('age');
-    $goal = $request->input('goal');
-    $amount = $request->input('amount');
-    $reoccurring = $request->input('reoccurring');
-    $contribution = $request->input('contribution');
-    $horizon_num = $request->input('horizon_num');
-    $horizon_type = $request->input('horizon_type');
 
-    $stocks = $request->input('stocks');
-    $options = $request->input('options');
-    $exTradeFunds = $request->input('exTradeFunds');
-    $secFutures = $request->input('secFutures');
-    $bonds = $request->input('bonds');
-    $mutualFunds = $request->input('mutualFunds');
-    $collegeSavings = $request->input('collegeSavings');
-    $commodityFutures = $request->input('commodityFutures');
-
-    $displayResults = Investment::all();
-
-    if($stocks != NULL) {
-      $displayResults->where('stocks', '=', $stocks);
-    }
-
-    return Response::json(['displayResults' => $displayResults]);
-}
+  }
 
   public function filterResults()
   {
 
-
-    return Response::json(['filterResults' => $filterResults]);
   }
-
 }
--- database/seeds/companiesTableSeeder.php
+++ database/seeds/companiesTableSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+use App\Company;
 
 class companiesTableSeeder extends Seeder
 {
@@ -11,6 +14,33 @@ class companiesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+      Company::insert(
+          [
+          'name' => 'Vanguard',
+          'description' => "Financial Advisor Services - Vanguard Personal Advisor Services, you get it all at a low cost—only 0.30% of your assets under management annually. Once you're enrolled, you can call or email a Vanguard advisor at your convenience. And with our easy-to-use videoconferencing service, you can also video chat with an advisor from the comfort of your own home. Simply schedule an appointment online, anytime.",
+          'website' => 'http://www.vanguard.com'
+          ],
+          [
+          'name' => 'Charles Schwab',
+          'description' => "Financial Advisor support available: robo-advisor services offered with a minimum investment of $5,000 and $0 advisory fees. Advisory services include Certified Financial Planner with a minimum investment of $25,000 and the following fee structure: First $100,000: 0.90%; Next $150,000: 0.70%; Next $250,000 0.50%; Next $500,000: 0.30%; and Over $1,000,000: 0.20%.<br/>
+          Special offers – 500 commission-free trades for two years. After this period, online equity trades will cost an industry-leading $4.95.<br/> 
+          No physical location available.",
+          'website' => 'http://www.charlesschwab.com'
+          ],
+          [
+          'name' => 'Ameritrade',
+          'description' => "Financial Advisor – Two advising options available. First, automated investing with low-cost, low minimum investment, with access to five goal-oriented ETF portfolios. The minimum investment is $5,000, and the advisory fee is 0.30% of assets under management. Second, a broader range of goal-oriented portfolios made up of mutual funds and ETFs, based on varying investment objectives and risk with ongoing rebalancing and monitoring. The minimum investment is $25,000, and the advisory fee is 0.75%-1.25% for the first $100,000.<br/>
+          Special offers – Trade free for 90-days and get up to $600 cash.<br/> 
+          Physical locations – 100+ branches nationwide",
+          'website' => 'http://www.ameritrade.com'
+          ],
+          [
+          'name' => 'Voya',
+          'description' => "Financial Advisors – Average fee of 0.30% of assets under management.<br/>
+          Special offers – None.<br/> 
+          Physical locations – Offices located on the East and West coast, but financial advisor services primarily conducted online with low-fee structure.",
+          'website' => 'http://www.voya.com'
+          ]
+    );
     }
 }
